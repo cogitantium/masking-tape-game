b1301067d15475709afd90c86063199397df97e5
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2019-10-22 22:28:09 +0200

    renamed variable

--- gen_turn_order.py
+++ gen_turn_order.py
@@ -1,2 +1,7 @@
-(lambda hashlib, sys, math, random: [not not ((_ == 0) and bool(print(f" #:         NAME | HASH\n {''.join(['=' for ___ in range(int(ord('2')/2))])}"))) or bool(print(__)) for _, __ in enumerate([f"{_:>2}: {__[math.floor(random.random())]:>12} | {__[1][:7]}" for _, __ in enumerate(sorted([(____, bool(sha.update(____.encode("UTF-8"))) or str(sha.hexdigest())[::-1]) for ____, sha in[(____, hashlib.new("sha256")) for ____ in sys.argv[1:]]],key=lambda __: __[int(len(__) / len(__))]))])])(__import__("hashlib"), __import__("sys"), __import__("math"), __import__("random"))
- 
+(lambda hashlib, sys, math, random: [not not ((_ == 0) and bool(
+    print(f" #:         NAME | HASH\n {''.join(['=' for ___ in range(int(ord('2')/2))])}"))) or bool(print(__)) for
+                                     _, __ in enumerate(
+        [f"{_:>2}: {__[math.floor(random.random())]:>12} | {__[1][:7]}" for _, __ in enumerate(sorted(
+            [(___, bool(sha.update(___.encode("UTF-8"))) or str(sha.hexdigest())[::-1]) for ___, sha in
+             [(___, hashlib.new("sha256")) for ___ in sys.argv[1:]]], key=lambda _: _[int(len(_) / len(_))]))])])(
+    __import__("hashlib"), __import__("sys"), __import__("math"), __import__("random"))
